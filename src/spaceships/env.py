6f2ea366aa9987e3a6441d1ae2c79a031140b358
Sagi Shadur <saroad2@gmail.com>
2022-08-07 21:29:55 +0300

    Add holes!

--- src/spaceships/env.py
+++ src/spaceships/env.py
@@ -11,6 +11,7 @@ from spaceships.direction import Direction
 
 class SpaceshipsEnv(gym.Env):
     star_reward = 10
+    enemy_death_reward = 5
     move_reward = 1
     lost_penalty = 2
 
@@ -19,11 +20,13 @@ class SpaceshipsEnv(gym.Env):
         size: int,
         enemy_move_chance: float = 0.33,
         turn_chance: float = 0,
+        holes_number: int = 10,
         screen: Optional[pygame.Surface] = None,
     ):
         self.size = size
         self.enemy_move_chance = enemy_move_chance
         self.turn_chance = turn_chance
+        self.holes_number = holes_number
         self.screen = screen
         self.font = (
             pygame.font.SysFont("ariel", 24) if self.screen is not None else None
@@ -32,17 +35,19 @@ class SpaceshipsEnv(gym.Env):
             0, 1, shape=(self.size, self.size), dtype=int
         )
         self.action_space = gym.spaces.Discrete(4)
-        self.player_location = self.random_location()
-        self.enemy_location = self.random_location()
-        self.star_location = self.random_location()
+        self.holes = self.generate_holes()
+        self.player_location = self.generate_player()
+        self.enemy_location = self.generate_npc()
+        self.star_location = self.generate_npc()
         self.moves = 0
         self.score = 0
+        self.enemy_kills = 0
         self.star_hits = 0
         self.slided = False
 
     @property
     def state_shape(self):
-        return self.size, self.size, 3
+        return self.size, self.size, 4
 
     @property
     def state(self):
@@ -55,6 +60,9 @@ class SpaceshipsEnv(gym.Env):
         state[sx, sy, 1] = 1
         ex, ey = self.enemy_location
         state[ex, ey, 2] = 1
+        for hole in self.holes:
+            hx, hy = hole
+            state[hx, hy, 3] = 1
         return state
 
     @property
@@ -86,7 +94,12 @@ class SpaceshipsEnv(gym.Env):
             return True
         if np.array_equal(self.player_location, self.enemy_location):
             return True
+        if self.in_hole(self.player_location):
+            return True
         return False
+
+    def in_hole(self, location):
+        return np.any([np.array_equal(location, hole) for hole in self.holes])
 
     def reset(
         self,
@@ -95,11 +108,13 @@ class SpaceshipsEnv(gym.Env):
         return_info: bool = False,
         options: Optional[dict] = None,
     ) -> Union[np.ndarray, Tuple[np.ndarray, dict]]:
-        self.player_location = self.random_location()
-        self.enemy_location = self.random_location()
-        self.star_location = self.random_location()
+        self.holes = self.generate_holes()
+        self.player_location = self.generate_player()
+        self.enemy_location = self.generate_npc()
+        self.star_location = self.generate_npc()
         self.moves = 0
         self.score = 0
+        self.enemy_kills = 0
         self.star_hits = 0
         self.slided = False
         return self.state
@@ -114,7 +129,11 @@ class SpaceshipsEnv(gym.Env):
         elif np.array_equal(self.player_location, self.star_location):
             self.star_hits += 1
             reward = self.star_reward
-            self.star_location = self.random_location()
+            self.star_location = self.generate_npc()
+        elif self.in_hole(self.enemy_location):
+            self.enemy_kills += 1
+            self.enemy_location = self.generate_npc()
+            reward = self.enemy_death_reward
         else:
             reward = (
                 self.move_reward
@@ -159,11 +178,28 @@ class SpaceshipsEnv(gym.Env):
         self.draw_block(location=self.star_location, color=YELLOW)
         self.draw_block(location=self.player_location, color=player_color)
         self.draw_block(location=self.enemy_location, color=RED)
+        for hole in self.holes:
+            self.draw_block(location=hole, color=BLACK)
         img = self.font.render(f"Score: {self.score:.2f}", False, BLACK)
         rect = img.get_rect()
         rect.midtop = (self.screen_width // 2, 0)
         self.screen.blit(img, rect)
         return None
+
+    def generate_holes(self):
+        return [self.random_location() for _ in range(self.holes_number)]
+
+    def generate_player(self):
+        location = self.random_location()
+        while self.in_hole(location):
+            location = self.random_location()
+        return location
+
+    def generate_npc(self):
+        location = self.random_location()
+        while self.in_hole(location) or np.array_equal(location, self.player_location):
+            location = self.random_location()
+        return location
 
     def random_location(self):
         return np.random.randint(self.size, size=2, dtype=int)
